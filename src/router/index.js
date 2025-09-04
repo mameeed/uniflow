3a3a0cd864850ba5039e888d0be2788ac727d1c9
mameeed <mukhammadumid@gmail.com>
2025-09-04 14:50:49 +0200

    Deploy additions

--- src/router/index.js
+++ src/router/index.js
@@ -19,7 +19,7 @@ const router = new VueRouter({
             return {x: 0, y: 0};
         }
     },
-    mode: "history",
+    mode: "hash",
     base: process.env.BASE_URL,
     routes,
 });
--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,8 @@ const {defineConfig} = require("@vue/cli-service");
 module.exports = defineConfig({
     transpileDependencies: true,
     // publicPath: process.env.NODE_ENV === "production" ? "/" + process.env.CI_PROJECT_NAME + "/" : "/",
+    publicPath: process.env.NODE_ENV === "production" ? "/uniflow/" : "/",
+
     // devServer: {
     //     proxy: "https://umkamatov.gitlab.io/unisale",
     // },
